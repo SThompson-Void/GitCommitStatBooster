58baddb7252d21d12112bd16db1cd172f0e0c2d1
SThompson <stephenthompsonformal@gmail.com>
2024-12-29 23:29:38 -0800

    v1.2 Added random commit between upper and lower bound functionality

--- main.cc
+++ main.cc
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <random>
 
-  /* replace below value with the max amount of times you want
-     to make commits per run of the program */
-const int kContributionCountPerRuntimeUpperBound = 100;
+// replace below value with the max amount of times you want to make commits per run of the program
+const int kContributionCountPerRuntimeUpperBound = 250;
+const int kContributionCountPerRuntimeLowerBound = 50;
 
 int main(int argc, const char** argv)
 {
+  //marsenne twister rand prng using kContributionCountPerRuntimeUpperBound as lim:
+  std::mt19937 generator(std::random_device{}());
+  std::uniform_int_distribution<int> distribution(kContributionCountPerRuntimeLowerBound, kContributionCountPerRuntimeUpperBound);
+  int randSelectedLimit = distribution(generator);
+
+  //cli app intro formatting
   std::cout <<"\n-------------------------------------------------------\n";
   std::cout << "\n\tWelcome to VOID's Git Commit Booster v1.1!\n";
   std::cout <<"\n-------------------------------------------------------\n";
-  std::cout <<"\n\n\tMaking " << kContributionCountPerRuntimeUpperBound << " commits...\n";
+  std::cout <<"\n\n\tMaking " << randSelectedLimit << " commits...\n";
 
-  for(int i = 0; i < kContributionCountPerRuntimeUpperBound; i++)
+  for(int i = 0; i < randSelectedLimit; i++)
   {
     // Open the file in read/write mode
     const char* fileName = "datadumpfile.txt";
@@ -44,10 +51,9 @@ int main(int argc, const char** argv)
   }
 
     std::system("git push --quiet");
-    std::cout <<"\n\n\t" << kContributionCountPerRuntimeUpperBound << " Pushed to Remote! \n\n\t(enter any char to exit)";
+    std::cout <<"\n\n\t" << randSelectedLimit << " Pushed to Remote! \n\n\t(enter any char to exit)";
     std::cout <<"\n-------------------------------------------------------\n";
     char temp{};
     std::cin >> temp;
   return 0;
-
 }
